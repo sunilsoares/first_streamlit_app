fdc7236e08a05fdc113921302efacf44e6147751
sunilsoares <151579334+sunilsoares@users.noreply.github.com>
2023-11-22 14:52:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
